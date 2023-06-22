3044460a6e743c348ca4cde0091bed8a3ec17460
stephbatliner <stephanie.batliner@digital.justice.gov.uk>
2023-06-22 16:24:58 +0100

    Added content v21 from old prototype

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,10 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+
+//require('./routes/routes.js')(router);
+require('./routes/v21.js')(router);
+
+//require('./views/'+version+'/routes/mainrouter.js')(router);
+module.exports = router;
